ebbf4a3a33710433a9d4cd241ecbd52073de67f3
James Gill <jamesgill347@gmail.com>
2020-10-28 18:00:11 -0400

    work done up to 6pm.

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,26 @@
 
 // Complete this algo
 const minJumps = arr => {
+    let currentIdx = 0;
+    let currentNumber = arr[currentIdx]
+    let counter = 0;
 
+    if (currentNumber >= arr.length) {
+        return 1
+    } else {
+        while (currentIdx + currentNumber < arr.length) {
+            for (let i = currentIdx + 1; i < currentIdx + currentNumber-1; i++) {
+                let maxNumber = 0;
+                if (arr[i] > maxNumber) {
+                    maxNumber = arr[i];
+                    currentIdx = maxNumber;
+                }
+            }
+            currentIdx += currentNumber;
+            counter +=1;
+        }
+    }
+    return counter;
 };
 
 module.exports = minJumps
